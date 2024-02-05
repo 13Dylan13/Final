08e6e6f3ca7fa86e5866d73abfacbee971d49980
13Dylan13 <80948211+13Dylan13@users.noreply.github.com>
2024-02-05 11:46:24 +0000

    loop moved to main

--- DataPipeline.py
+++ DataPipeline.py
@@ -2,6 +2,7 @@ from database import database_connection
 from database import select_all
 from app import getoutcomes
 from app import getVTM
+import datetime
 import os
 
 
@@ -10,9 +11,13 @@ subday = 15 #number of historic extracts to check for
 cwd = os.chdir(r'C:/Users/DR2806/OneDrive - Capita Plc/P Drive/DataScience/Advanced/final')
 db = database_connection() 
 checkfile = select_all(db, 'extractedfiles')   
-getoutcomes(db,subday,checkfile)
-getVTM(db,subday,checkfile)
+#could add loop for historic days here
+n=0
+while n<subday:
+    startdate = datetime.datetime.today() - datetime.timedelta(days=n)
+    getoutcomes(db,startdate,checkfile)
+    getVTM(db,startdate,checkfile)
+    n=n+1
 db.close()
-
 
 
